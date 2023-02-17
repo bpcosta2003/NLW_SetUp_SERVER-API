9648c6e96f196193d0ad3d6846b6f902fe7e95c7
bpcosta2003 <bruno.p.costa.2003@gmail.com>
2023-02-17 14:52:14 -0300

    verceljson

---
+++ index.ts
@@ -0,0 +1,18 @@
+import cors from "@fastify/cors";
+import Fastify from "fastify";
+import {main} from "./src/routes";
+require("dotenv").config();
+require("./src/server");
+
+const app = Fastify();
+
+app.register(cors);
+app.register(main);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server running on port 3333");
+  });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-import cors from "@fastify/cors";
-import Fastify from "fastify";
-import {main} from "./routes";
-
 const mongoose = require("mongoose");
 const MONGODB_URI =
   "mongodb+srv://" +
@@ -11,18 +7,6 @@ const MONGODB_URI =
   "@habitstracker.vu0y9rb.mongodb.net/" +
   process.env.MONGODB_DATABASE +
   "?retryWrites=true&w=majority";
-const app = Fastify();
-
-app.register(cors);
-app.register(main);
-
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log("Server running on port 3333");
-  });
 
 mongoose
   .connect(MONGODB_URI, {
