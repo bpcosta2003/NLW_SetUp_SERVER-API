b6bc6cadf33b382fc32f9474e24a090b8ba50c05
bpcosta2003 <bruno.p.costa.2003@gmail.com>
2023-02-20 15:21:44 -0300

    express

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
-import cors from "@fastify/cors";
+import cors from "cors";
 import Fastify from "fastify";
 import {main} from "./src/routes";
 // require("dotenv").config();
 // require("./src/server");
 
-const app = Fastify();
+const express = require("express");
+const app = express();
+const port = 3333;
 
-app.register(cors);
-app.register(main);
+app.use(cors());
+app.use(main);
 
 app
   .listen({
-    port: 3333,
+    port: port,
   })
   .then(() => {
     console.log("Server running on port 3333");
